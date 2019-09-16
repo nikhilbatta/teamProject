808d664d569031b725f4b7472547915ac1d50bd0
Student <example@example.com>
2019-09-16 15:50:40 -0700

    Add API call, addedHTMLtemplate recipeOBJ page can be deleted.

--- src/main.js
+++ src/main.js
@@ -4,3 +4,19 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import {} from './project';
+import {RecipeByIngredients} from './mainingredient.js'
+
+$(document).ready(function(){
+  var arr = ["chicken"]
+  callRecipeAPI(arr)
+
+})
+function callRecipeAPI(ingredients){
+  console.log("itsworking")
+ let recipeCall = new RecipeByIngredients();
+ recipeCall.getIdByIngredient(ingredients).then(displayRecipe())// display function, if error then we call a error function)
+}
+function displayRecipe(){
+ let recipe = JSON.parse(response)
+ $('#body').append(`${recipe[0].title}`)
+}
