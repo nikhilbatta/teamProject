c336863c4b7fdae1c7f99c2d27e1675e1bf705cb
Student <example@example.com>
2019-09-16 20:28:50 -0700

    started display foreach Logic and also fixed api call

--- src/mainingredient.js
+++ src/mainingredient.js
@@ -4,7 +4,7 @@ export class RecipeByIngredients{
       let request = new XMLHttpRequest();
       let url = `https://api.spoonacular.com/recipes/complexSearch?query=${ingredients[0]}`;
       for (var i =1; i<ingredients.length;i++){
-        url+= `,+${ingredient[i]}`;
+        url+= `,+${ingredients[i]}`;
       }
       url += `&addRecipeInformation=true&fillIngredients=true&apiKey=${process.env.apiKey}`;
       console.log(url);
@@ -16,6 +16,8 @@ export class RecipeByIngredients{
         reject(Error(request.statusText))
       }
     }
+    request.open("GET", url, true);
+    request.send();
   });
 }
 }
